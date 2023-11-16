1b55d2a026c513e8f936d2bc82e683a235ef9cda
Helen Feng (VM) <fenghelen@g.ucla.edu>
2023-11-15 16:19:09 -0800

    implemented v1

--- hash-table-v1.c
+++ hash-table-v1.c
@@ -20,6 +20,7 @@ struct hash_table_entry {
 };
 
 struct hash_table_v1 {
+	pthread_mutex_t lock;
 	struct hash_table_entry entries[HASH_TABLE_CAPACITY];
 };
 
@@ -30,6 +31,10 @@ struct hash_table_v1 *hash_table_v1_create()
 	for (size_t i = 0; i < HASH_TABLE_CAPACITY; ++i) {
 		struct hash_table_entry *entry = &hash_table->entries[i];
 		SLIST_INIT(&entry->list_head);
+	}
+	if (pthread_mutex_init(&hash_table->lock, NULL) != 0)
+	{
+		exit(-1);
 	}
 	return hash_table;
 }
@@ -72,9 +77,12 @@ void hash_table_v1_add_entry(struct hash_table_v1 *hash_table,
                              const char *key,
                              uint32_t value)
 {
+	// lock here so each thread sees an updated version of the hashtable
+	pthread_mutex_lock(&(hash_table->lock));
+
 	struct hash_table_entry *hash_table_entry = get_hash_table_entry(hash_table, key);
-	struct list_head *list_head = &hash_table_entry->list_head;
-	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
+	struct list_head *list_head = &hash_table_entry->list_head; // locate head
+	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head); // getlist entry
 
 	/* Update the value if it already exists */
 	if (list_entry != NULL) {
@@ -86,6 +94,8 @@ void hash_table_v1_add_entry(struct hash_table_v1 *hash_table,
 	list_entry->key = key;
 	list_entry->value = value;
 	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
+
+	pthread_mutex_unlock(&(hash_table->lock));
 }
 
 uint32_t hash_table_v1_get_value(struct hash_table_v1 *hash_table,
@@ -100,6 +110,7 @@ uint32_t hash_table_v1_get_value(struct hash_table_v1 *hash_table,
 
 void hash_table_v1_destroy(struct hash_table_v1 *hash_table)
 {
+	pthread_mutex_destroy(&(hash_table->lock));
 	for (size_t i = 0; i < HASH_TABLE_CAPACITY; ++i) {
 		struct hash_table_entry *entry = &hash_table->entries[i];
 		struct list_head *list_head = &entry->list_head;
